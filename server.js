ae71af3fecd6ba5e69dba1a9a2d29254490160bc
Thomas Liu <tsmliu213@gmail.com>
2017-01-06 22:01:00 +0000

    mongodb update changed addToSet

--- server.js
+++ server.js
@@ -8,15 +8,40 @@ app.use('/', express.static(__dirname + '/public'));
 app.get('/api', function(req, res) {
     var query = req.query.q;
     var offset = req.query.offset;
+    addVisited(query);
     bing.imgSearch(query, offset, res);
-    
-    mongodb.connect(url, function(err, db){
-       
-       db.close(); 
-    });
+
 });
 app.get('/api/latest', function(req, res) {
     
 });
 app.listen(process.env.PORT || 8080);
+
+function addVisited(query) {
+    mongodb.connect(url, function(err, db) {
+        if(err) console.log(err);
+        else {
+            var date = new Date();
+            var queries = db.collection('queries');
+            queries.findOne({visited: "visited"}, function(err, document) {
+                if(err) console.log(err);
+                else if(document) {
+                    var visit = {"query": query, "time_stamp": date.toISOString()};
+                    queries.update({visited: "visited"}, {$addToSet: {latest: visit}}, function(err, result) {
+                        if(err) console.log(err)
+                        else db.close();
+                    });
+                    console.log("add to existing list");
+                }
+                else {
+                    queries.insert({visited: "visited", latest: [{"query": query, "time_stamp": date.toISOString()}]}, function(err, result) {
+                        if(err) console.log(err)
+                        else db.close();
+                    });
+                    console.log("first query inserted");
+                }
+            });
+        }
+    });
+}
 
